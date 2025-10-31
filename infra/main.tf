81fdaaa2ddb8ef110ad33b282f9a2f125975b8ad
Vinay Kumar <vinaymavi@gmail.com>
2025-10-31 18:28:25 +0530

    feat: Refactor Terraform configuration by separating provider definitions into a new file and updating repository settings

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
-
-# Configure the Google Cloud Provider
-provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-  zone    = var.gcp_zone
-}
-
-# Configure the GitHub Provider
-provider "github" {
-  token = var.github_token
-  owner = var.github_owner
-}
-
 
 # Example GitHub repository
 resource "github_repository" "html_to_images" {
@@ -38,8 +10,7 @@ resource "github_repository" "html_to_images" {
   has_projects = true
   has_wiki     = true
 
-  auto_init = true
+  auto_init = false
 
-  gitignore_template = "node.js"
-  license_template   = "mit"
+  license_template = "mit"
 }
---
+++ infra/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~>7.7.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.project_name
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
